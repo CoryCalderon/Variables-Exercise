d737d3a82179e2109255b88039debed9c12c40ea
Cory <corycalderon95@gmail.com>
2024-04-30 11:25:56 -0400

    Added variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Cory";
+            int myAge = 29;
+            char firstInitial = 'C';
+            bool isHungry = true;
+            double myWeight = 220;
+            decimal myHeight = 70.0i;
+
+            Console.WriteLine($"Hello! My name is {myName}. I am {myAge} years old.")
+
+
+
         }
     }
 }
